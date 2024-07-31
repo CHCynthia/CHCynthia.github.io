1918189cbec3ccf3142021ded4c2d47897c3f885
CHCynthia <129368566+CHCynthia@users.noreply.github.com>
2024-07-31 17:52:08 +0800

    0731test

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
 const bookElement = document.getElementById('book');
 
 const pageFlip = new St.PageFlip(bookElement, {
-    width: 1160,
-    height: 780,
-    size: 'fixed',
+    width: 2400,
+    height: 1600,
+    size: "fixed",
     showCover: true,
     usePortrait: true,
+    singlePage: true,
+    autoSize: true,
+    mobileScrollSupport: false,
 });
 
 const images = [];
-for (let i = 1; i <= 2; i++) {
-    const pageNum = i.toString().padStart(2, '0');
-    images.push(`images/${pageNum}.gif`);
-}
-for (let i = 3; i <= 35; i++) {
+for (let i = 1; i <= 35; i++) {
     const pageNum = i.toString().padStart(2, '0');
     images.push(`images/${pageNum}.jpg`);
 }
