4d4bc915a0a3ab6d0a596e854c02e8cb50f29a31
CHCynthia <129368566+CHCynthia@users.noreply.github.com>
2024-08-05 14:06:17 +0800

    test

--- main.js
+++ main.js
@@ -1,47 +1,52 @@
-const bookElement = document.getElementById('book');
+document.addEventListener('DOMContentLoaded', () => {
+    const bookElement = document.getElementById('book');
 
-const pageFlip = new St.PageFlip(bookElement, {
-    width: 1400, // Adjusted to 50% of original width
-    height: 800, // Adjusted to 50% of original height
-    size: "fixed",
-    showCover: true,
-    usePortrait: true,
-    singlePage: true,
-    autoSize: true,
-    mobileScrollSupport: false,
+    const pageFlip = new St.PageFlip(bookElement, {
+        width: 1400,
+        height: 800,
+        size: "fixed",
+        showCover: true,
+        usePortrait: true,
+        singlePage: true,
+        autoSize: true,
+        mobileScrollSupport: false,
+    });
+
+    const images = [];
+
+    for (let i = 1; i <= 35; i++) {
+        const pageNum = i.toString().padStart(2, '0');
+        images.push(`images/${pageNum}.jpg`); // 修正圖片路徑
+    }
+
+    pageFlip.loadFromImages(images).then(() => {
+        document.querySelector(".page-total").innerText = pageFlip.getPageCount();
+        document.querySelector(".page-current").innerText = pageFlip.getCurrentPage(); // 設置當前頁面
+
+        document.querySelector(".btn-prev").addEventListener("click", () => {
+            pageFlip.flipPrev();
+        });
+
+        document.querySelector(".btn-next").addEventListener("click", () => {
+            pageFlip.flipNext();
+        });
+
+        pageFlip.on("flip", (e) => {
+            document.querySelector(".page-current").innerText = e.data + 1;
+        });
+
+        pageFlip.on("changeState", (e) => {
+            const stateElement = document.querySelector(".page-state");
+            if (stateElement) {
+                stateElement.innerText = e.data;
+            }
+        });
+
+        pageFlip.on("changeOrientation", (e) => {
+            const orientationElement = document.querySelector(".page-orientation");
+            if (orientationElement) {
+                orientationElement.innerText = e.data;
+            }
+        });
+    });
 });
-
-const images = [];
-
-for (let i = 1; i <= 35; i++) {
-    const pageNum = i.toString().padStart(2, '0');
-    images.push(`images/${pageNum}.jpg`);
-}
-pageFlip.loadFromImages(images);
-
-document.querySelector(".page-total").innerText = pageFlip.getPageCount();
-    document.querySelector(
-        ".page-orientation"
-    ).innerText = pageFlip.getOrientation();
-
-    document.querySelector(".btn-prev").addEventListener("click", () => {
-        pageFlip.flipPrev(); // Turn to the previous page (with animation)
-    });
-
-    document.querySelector(".btn-next").addEventListener("click", () => {
-        pageFlip.flipNext(); // Turn to the next page (with animation)
-    });
-    pageFlip.on("flip", (e) => {
-        document.querySelector(".page-current").innerText = e.data + 1;
-    });
-
-    // triggered when the state of the book changes
-    pageFlip.on("changeState", (e) => {
-        document.querySelector(".page-state").innerText = e.data;
-    });
-
-    // triggered when page orientation changes
-    pageFlip.on("changeOrientation", (e) => {
-        document.querySelector(".page-orientation").innerText = e.data;
-    });
-
