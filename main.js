8337bd3aeba5910746fca6ae025029093850df46
CHCynthia <129368566+CHCynthia@users.noreply.github.com>
2024-08-05 14:08:52 +0800

    test

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const bookElement = document.getElementById('book');
 
     const pageFlip = new St.PageFlip(bookElement, {
-        width: 1400,
+        width: 1300,
         height: 800,
         size: "fixed",
         showCover: true,
