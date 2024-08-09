c8881cd6a21f825a095f44d672b156f81c96f359
CHCynthia <129368566+CHCynthia@users.noreply.github.com>
2024-08-09 16:09:35 +0800

    0809test

--- main.js
+++ main.js
@@ -1,26 +1,27 @@
-// 獲取書本容器
 const bookElement = document.getElementById('book');
 
-// 創建 PageFlip 對象
 const pageFlip = new St.PageFlip(bookElement, {
-    width: 1200,  // 容器的寬度
-    height: 1600, // 容器的高度
-    size: 'fixed', // 固定大小
-    showCover: true, // 顯示封面 
-    drawShadow: false,
-    maxShadowOpacity:0,
+    width: 2400,
+    height: 1600,
+    size: "fixed",
+    showCover: true,
+    usePortrait: true,
+    singlePage: true,
+    autoSize: true,
+    mobileScrollSupport: false,
 });
 
-// 加載圖片
 const images = [];
-for (let i = 1; i <= 5; i++) {
-    const pageNum = i.toString().padStart(2, '0'); // 將數字轉為兩位數的字符串
+for (let i = 0; i <= 36; i++) {
+    const pageNum = i.toString().padStart(2, '0');
     images.push(`images/${pageNum}.jpg`);
 }
-
 pageFlip.loadFromImages(images);
 
-// 當窗口大小改變時更新 PageFlip
-window.addEventListener('resize', () => {
-    pageFlip.updateFromImages(images);
+document.getElementById('prevPage').addEventListener('click', () => {
+    pageFlip.flipPrev();
+});
+
+document.getElementById('nextPage').addEventListener('click', () => {
+    pageFlip.flipNext();
 });
